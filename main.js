b5835db312a656d9bf26c7b9214049c454f99672
demon-monkey <74807858+demon-monkey@users.noreply.github.com>
2021-10-23 11:02:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var speechRecognition=window.webkitSpeechRecognition;
+var recognition=new speechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function(event){
+console.log(event);
+var content=event.results[0][0].transcript;
+console.log(content);
+document.getElementById("textbox").innerHTML=content;
+}
