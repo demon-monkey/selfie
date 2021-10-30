2bc396e190eb0a3e4d8cf220eed181ca9b3e87ef
demon-monkey <74807858+demon-monkey@users.noreply.github.com>
2021-10-30 10:51:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,21 @@ console.log(event);
 var content=event.results[0][0].transcript;
 console.log(content);
 document.getElementById("textbox").innerHTML=content;
+speakInfo()
 }
+
+function speakInfo(){
+var Synth= window.speechSynthesis;
+speakdata=document.getElementById("textbox").value;
+var utter=new SpeechSynthesisUtterance(speakdata);
+utter.lang="en-GB";
+utter.pitch=10.2;
+Synth.speak(utter)
+Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,height:250,image_format:'jpeg',jpeg_quality:100
+});
+
+camera=document.getElementById("camera");
